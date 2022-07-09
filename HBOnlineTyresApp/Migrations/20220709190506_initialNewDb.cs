d0bf9bd31c499f9a78a64f35b08b0bbfc7ec8c64
Chang062 <andrewolf_@live.com>
2022-07-09 12:10:59 -0700

    Delete confirmation

---
+++ HBOnlineTyresApp/Migrations/20220709190506_initialNewDb.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HBOnlineTyresApp.Migrations
+{
+    public partial class initialNewDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Tyres_CategoryId",
+                table: "Tyres",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tyres_Categories_CategoryId",
+                table: "Tyres",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tyres_Categories_CategoryId",
+                table: "Tyres");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tyres_CategoryId",
+                table: "Tyres");
+        }
+    }
+}
